22da67ed25a3a11310a3f2c5271bbab2a902fa2d
LuisCastellanos <luisda.betancourt@gmail.com>
2022-09-03 19:41:10 -0600

    subiendo tarea

---
+++ src/tarea2/Tarea2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea2;
+
+/**
+ *
+ * @author Luisda
+ */
+public class Tarea2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        InicioSesion Ventana1 = new InicioSesion();
+        Ventana1.setVisible(true);
+        // TODO code application logic here
+    }
+
+}
